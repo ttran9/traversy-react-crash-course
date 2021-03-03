372d188ac75935118cee0eaa7a927988a10e35d4
ttran9 <toddtran9@gmail.com>
2021-03-02 21:11:15 -0800

    adding in task component.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Task from './Task';
+
 
 
 const Tasks = ({tasks}) => {
     return (
         <div>
-        {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        {tasks.map((task) => (<Task key={task.id} task={task}/>))}
         </div>
     )
 }
