34d556d5a0dc679c7e27bce053d8a2d2d0cfeba9
ttran9 <toddtran9@gmail.com>
2021-03-02 21:06:28 -0800

    using hook at a global level.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import "./index.css";
+import { useState }  from 'react'
 
 function App() { 
+    const [tasks, setTasks] = useState([
+        {
+            "id": 1,
+            "text": "Doctors Appointment",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+          },
+          {
+            "id": 2,
+            "text": "Meeting at School",
+            "day": "Feb 6th at 1:30pm",
+            "reminder": true
+          },
+          {
+              id: 3,
+              text: 'Food Shopping',
+              day: 'Feb 5th at 2:30pm',
+              reminder: false,
+          }
+    ])
 
     return (
         <div className="container">
             <h1>Hello From React</h1>
             <Header />
             <h2>Lets test!!!</h2>
-            <Tasks/>
+            <Tasks tasks={tasks} />
             {/* <Header title = {1}/> */}
             {/* <Header title='custom title tracker'/> */}
         </div>  
