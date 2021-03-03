357ceec1dca23f1218a034edc897d94116df8a84
ttran9 <toddtran9@gmail.com>
2021-03-02 22:47:09 -0800

    toggling button based on state values.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd }) => {
 // const Header = (props) => {
-
-    const onClick = () => {
-        console.log('click');
-    }
-
     return (
         <div> 
             <header className='header'>
                 <h1>{title}</h1>
-                <Button color = 'green' text='Add' onClick={onClick}/>
+                <Button 
+                    color = {showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'} 
+                    onClick={onAdd}/>
                 {/* <Button color = 'red' text='Hello 1!'/> */}
                 {/* <Button color = 'blue' text='Hello 3!'/> */}
                 {/* <h1>{props.title}</h1> */}
