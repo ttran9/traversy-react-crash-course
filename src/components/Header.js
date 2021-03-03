19c5a4e71be84db950c99ff6eae3f6828ca176d3
ttran9 <toddtran9@gmail.com>
2021-03-02 19:59:30 -0800

    adding Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Header = () => {
+    return (
+        <div> 
+        <header>
+        <h1>Task</h1>
+        </header>
+        </div>
+    )
+}
+
+export default Header
+
