08d0e8986e2a7055eaf823a090040d326aa53b22
ttran9 <toddtran9@gmail.com>
2021-03-03 00:13:10 -0800

    toggle reminder for task.

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
         return data;
     }
 
+    // Fetch Task
+    const fetchTask = async (id) => {
+        const res = await fetch(`${endPoint}${id}`);
+        const data = await res.json();
+        return data;
+    }
+
     // Add Task
     const addTask = async (task) => {
         const res = await fetch(`${endPoint}`, {
@@ -56,10 +63,26 @@ function App() {
     }
 
     // Toggle Reminder
-    const toggleReminder = (id) => {
-        setTasks(tasks.map((task) => task.id === id ? 
-                    { ...task, reminder: !task.reminder } : 
-                    task))
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id);
+        const updatedTask = {...taskToToggle, reminder:!taskToToggle.reminder};
+
+        const res = await fetch(`${endPoint}${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedTask)
+        })
+
+        const data = await res.json();
+
+        setTasks(
+            tasks.map((task) => 
+                task.id === id ? { ...task, reminder: 
+                    data.reminder } : task
+            )
+        )
     }
 
     return (
