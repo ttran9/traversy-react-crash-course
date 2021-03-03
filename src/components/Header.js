88d73cfbffde6bf9175fe743cfa50c81968948e5
ttran9 <toddtran9@gmail.com>
2021-03-02 20:21:01 -0800

    demonstrating re-usable components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import Button from './Button';
 
-// const Header = ({title}) => {
-const Header = (props) => {
+const Header = ({title}) => {
+// const Header = (props) => {
     return (
         <div> 
-        <header>
-        {/* <h1>{title}</h1> */}
-        <h1>{props.title}</h1>
-        </header>
+            <header className='header'>
+                <h1>{title}</h1>
+                <Button color = 'green' text='Add'/>
+                {/* <Button color = 'red' text='Hello 1!'/> */}
+                {/* <Button color = 'blue' text='Hello 3!'/> */}
+                {/* <h1>{props.title}</h1> */}
+                {/* <h1 style={headingStyle}>{props.title}</h1> */}
+            </header>
         </div>
     )
 }
@@ -19,6 +24,12 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired
+}
+
+// CSS in JS.
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black'
 }
 
 export default Header
